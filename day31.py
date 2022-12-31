5a127c8dbccd1a9ad01a16b717e586d78c58965b
puja sahu <100419841+pujasahu27@users.noreply.github.com>
2022-12-31 20:05:56 +0530

    Add files via upload

---
+++ day31.py
@@ -0,0 +1,19 @@
+#sets
+
+k = {2,4,2,6}
+print(k)
+
+info={"Neha", 20, True, 2.2,19}
+print(info)
+
+
+for value in info:
+    print(value)
+
+"""harry = {}
+print(type(harry))"""
+
+harry = set()
+print(type(harry))
+
+
