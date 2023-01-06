7d941d650c6f76ded986c0fc64d585c55aa6bd05
puja sahu <100419841+pujasahu27@users.noreply.github.com>
2023-01-06 17:28:01 +0530

    Add files via upload

---
+++ day37.py
@@ -0,0 +1,24 @@
+#finally keyword
+"""try:
+    l=[1,5,6,7]
+    i=int(input("Enter the index:"))
+    print(l[i])
+except:
+    print("some error occured")
+finally:
+    print("I am always executed")"""
+
+def func1():
+  try:
+    l=[1,5,6,7]
+    i=int(input("Enter the index:"))
+    print(l[i])
+    return 1
+  except:
+    print("some error occured")
+    return 0
+  finally:
+    print("I am always executed")
+
+x=func1()
+print(x)
