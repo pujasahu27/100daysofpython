6c57004e219b0e9101868572063da8836272f208
puja sahu <100419841+pujasahu27@users.noreply.github.com>
2023-01-08 13:54:13 +0530

    Add files via upload

---
+++ day38.py
@@ -0,0 +1,22 @@
+#Custom error
+"""salary = int(input("Enter salary amount: "))
+if not 2000 < salary < 5000:
+    raise ValueError("Not a valid salary")"""
+
+
+
+a = int(input("Enter any value between 5 and 9"))
+
+if(a<5  or a>9):
+  raise  ValueError("Value should be between 5 and 9")
+
+#syntax
+"""class CustomError(Exception):
+  # code ...
+  pass
+
+try:
+  # code ...
+
+except CustomError:
+  # code..."""
