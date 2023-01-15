e82657721f11405f4d4f05126670b6fd7a0612e6
puja sahu <100419841+pujasahu27@users.noreply.github.com>
2023-01-15 22:24:56 +0530

    Add files via upload

---
+++ day45.py
@@ -0,0 +1,4 @@
+#if __name__ == "__main__" in Python
+import day45_2
+
+day45_2.welcome()
---
+++ day45_2.py
@@ -0,0 +1,6 @@
+#if __name__ == "__main__" in Python
+def welcome():
+    print("hey you are welcome")
+print(__name__)    
+if __name__=="__main__":
+  welcome()    
