6ee28b311e5edc9cc01a3732db385c76a5b8b55f
puja sahu <100419841+pujasahu27@users.noreply.github.com>
2023-02-06 22:24:01 +0530

    Add files via upload

---
+++ day54.py
@@ -0,0 +1,13 @@
+#is vs ==
+a=[1,2,3]
+b=[1,2,4,3]
+
+print(a is b)#exact loctzion of object in library
+print(a==b)#value
+
+
+a=None
+b=None
+print(a is b)#exact loctzion of object in library
+print(a is None)
+print(a==b)#value
