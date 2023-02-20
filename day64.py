673b0de4bf7d0048993b1d6a55a5cfd15158ee08
puja sahu <100419841+pujasahu27@users.noreply.github.com>
2023-02-20 23:46:58 +0530

    Add files via upload

---
+++ day64.py
@@ -0,0 +1,21 @@
+#Library Management Software in Python
+class Library:
+  def __init__(self):
+    self.noBooks = 0
+    self.books = []
+    
+  def addBook(self, book):
+    self.books.append(book)
+    self.noBooks = len(self.books)
+
+  def showInfo(self):
+    print(f"The library has {self.noBooks} books. The books are")
+    for book in self.books:
+      print(book)
+
+
+l1 = Library()
+l1.addBook("Harry Potter")
+l1.addBook("Python")
+l1.addBook("Computer")
+l1.showInfo()
