a2fd5ba4aa811ebf56071595c07d59a2fe13e930
puja sahu <100419841+pujasahu27@users.noreply.github.com>
2023-02-22 01:10:18 +0530

    Add files via upload

---
+++ day67.py
@@ -0,0 +1,8 @@
+#String Split and Join
+def print_full_name(first, last):
+  print(f"Hello {first} {last}! You just delved into python.") 
+
+if __name__ == '__main__':
+    first_name = input("Rose")
+    last_name = input("Taylor")
+    print_full_name(first_name, last_name)
